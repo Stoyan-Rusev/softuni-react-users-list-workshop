04fbe1b46fc955cc069d8549b51a41fa525da4b7
Stoyan-Rusev <stoyan.rusevv@gmail.com>
2025-10-17 15:28:52 +0300

    refactor: components divide

---
+++ client/src/components/Main.jsx
@@ -0,0 +1,12 @@
+import Section from "./Section";
+import UserDetails from "./UserDetails";
+
+
+export default function Main() {
+    return (
+        <main className="main">
+            <Section />
+            <UserDetails />
+        </main>
+    );
+};
